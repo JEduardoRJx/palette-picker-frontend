89940b7d8af284f9ccd555b086ee6ef1636a8d34
dawnlunacy <rudd.lacy@gmail.com>
2019-12-09 15:16:31 -0700

    Add header to app

--- tone-zone-fe/src/App.js
+++ tone-zone-fe/src/App.js
@@ -1,29 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.scss';
+import { Header } from '../src/containers/Header/Header'
 // import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello.
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+export class App extends Component {
+
+  async componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+} 
+
 
 export default App;
 
---
+++ tone-zone-fe/src/containers/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// import PropTypes from 'prop-types';
+
+export const Header = () => {
+  return(
+    <header>
+      <h1> Tone Zone </h1>
+    </header>
+  )
+}
+
+export default Header;

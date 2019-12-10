28db1baa472f8998bfec8b309eb61f156fde955c
dawnlunacy <rudd.lacy@gmail.com>
2019-12-09 22:38:54 -0700

    ColorCards inside of ColorContainer are responsive to adjustments horizontally of screen size

--- tone-zone-fe/src/containers/Main/Main.js
+++ tone-zone-fe/src/containers/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ProjectForm } from '../ProjectForm/ProjectForm';
 import { ActionContainer } from '../ActionContainer/ActionContainer';
 import { PaletteForm } from '../PaletteForm/PaletteForm';
+import './Main.scss';
 // import PropTypes from 'prop-types';
 
 export const Main = () => {

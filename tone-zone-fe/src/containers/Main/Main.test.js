9917fe8f7ab278f270f619fe704e03df0655ff33
dawnlunacy <rudd.lacy@gmail.com>
2019-12-09 20:50:49 -0700

    Add main component where drop downs will live

---
+++ tone-zone-fe/src/containers/Main/Main.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+export const Main = () => {
+
+  return (
+    <section className="main-section"> 
+
+    </section>
+  )
+}
+
+export default Main;

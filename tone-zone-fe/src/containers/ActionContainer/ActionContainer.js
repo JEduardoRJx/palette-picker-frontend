50ff8343f98ca22340a66fb166dd574530dafa37
dawnlunacy <rudd.lacy@gmail.com>
2019-12-09 23:07:59 -0700

    Connect sass to all components

--- tone-zone-fe/src/containers/ActionContainer/ActionContainer.js
+++ tone-zone-fe/src/containers/ActionContainer/ActionContainer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './ActionContainer.scss';
 
 export const ActionContainer = () => {
   return (
-    <section>
+    <section className="action-container">
       <p> Logo </p>
       <button className="randomize-btn"> Randomize </button>
       <button className="save-btn"> SAVE </button>

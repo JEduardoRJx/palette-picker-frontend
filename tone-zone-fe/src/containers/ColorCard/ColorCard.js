f43de2aa4b9e4e3a6fa4207d7d9915294a91bad0
dawnlunacy <rudd.lacy@gmail.com>
2019-12-09 22:23:21 -0700

    Add basic ColorCard and display via CardContainer

---
+++ tone-zone-fe/src/containers/ColorCard/ColorCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ColorCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ColorCard;
--- tone-zone-fe/src/containers/ColorContainer/ColorContainer.js
+++ tone-zone-fe/src/containers/ColorContainer/ColorContainer.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { ColorCard } from '../ColorCard/ColorCard';
 export const ColorContainer = () => {
   return (
     <section className="color-container">
-      
+      <ColorCard />
+      <ColorCard />
+      <ColorCard />
+      <ColorCard />
+      <ColorCard />
     </section>
   )
 }

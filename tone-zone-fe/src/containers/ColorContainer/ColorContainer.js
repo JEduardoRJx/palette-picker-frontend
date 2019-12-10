3373dcba4076fb7829160de81569463a3a2881e0
dawnlunacy <rudd.lacy@gmail.com>
2019-12-09 22:19:21 -0700

    Add basic colorContainer and connect to DOM

---
+++ tone-zone-fe/src/containers/ColorContainer/ColorContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ColorContainer = () => {
+  return (
+    <section className="color-container">
+      
+    </section>
+  )
+}
+
+export default ColorContainer;

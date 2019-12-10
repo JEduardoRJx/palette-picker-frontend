0dff589c8b5d1f1d103e2589a2a341c8027f0d5d
dawnlunacy <rudd.lacy@gmail.com>
2019-12-09 21:12:32 -0700

    Add DropDown Container with a drop down selection menu for projects

---
+++ tone-zone-fe/src/containers/DropDownContainer/DropDownContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const DropDown = () => {
+  return (
+    <section>
+    <select>
+      <option value="Project Name"> Project Name </option>
+      <option value="Project Name 1"> Project 1 </option>
+      <option value="Project Name 2"> Project 2 </option>
+    </select>
+  </section>
+  )
+}
+
+export default DropDown;
---
+++ tone-zone-fe/src/containers/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { DropDown } from '../DropDownContainer/DropDownContainer';
+// import PropTypes from 'prop-types';
+
+export const Main = () => {
+
+  return (
+    <section className="main-section"> 
+    
+    <DropDown />
+    </section>
+  )
+}
+
+export default Main;

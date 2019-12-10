b2d317b7ebfc4bb85f7d9be4c4c958a46a086c2b
dawnlunacy <rudd.lacy@gmail.com>
2019-12-09 21:58:05 -0700

    Fix export to allow ActionContainer to be displayed from Main

--- tone-zone-fe/src/containers/ActionContainer/ActionContainer.js
+++ tone-zone-fe/src/containers/ActionContainer/ActionContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ActionContainer = () => {
+export const ActionContainer = () => {
   return (
     <section>
       <p> Logo </p>
--- tone-zone-fe/src/containers/Main/Main.js
+++ tone-zone-fe/src/containers/Main/Main.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { ProjectForm } from '../ProjectForm/ProjectForm';
+import { ActionContainer } from '../ActionContainer/ActionContainer';
 // import PropTypes from 'prop-types';
 
 export const Main = () => {
 
   return (
     <section className="main-section"> 
-  
     <ProjectForm />
+    <ActionContainer />
     </section>
   )
 }

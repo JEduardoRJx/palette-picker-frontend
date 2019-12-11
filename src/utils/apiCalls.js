6e2e2efc5ddc465089b3c7c3d65826589ee5b634
dawnlunacy <rudd.lacy@gmail.com>
2019-12-10 20:06:17 -0700

    Added a utils folder with apiCalls inside and attempted our first fetch

---
+++ src/utils/apiCalls.js
@@ -0,0 +1,10 @@
+
+export const fetchData = async (url) => {
+  const response = await fetch(url)
+  if (response.ok) {
+    const data = await response.json()
+    return data;
+  } else {
+    throw Error(response.statusText)
+  }
+}

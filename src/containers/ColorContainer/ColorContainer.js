fb2b847351fa6ad2c251f2971dfb29f7226f82c1
Eduardo <jeduardorjx@gmail.com>
2019-12-10 18:24:24 -0700

    Format component to map through all the colors array and render ColorCard

--- src/containers/ColorContainer/ColorContainer.js
+++ src/containers/ColorContainer/ColorContainer.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { ColorCard } from '../ColorCard/ColorCard';
 import './ColorContainer.scss';
 
-export const ColorContainer = () => {
+export const ColorContainer = ({ colors }) => {
+  const allColorCards = colors.map(color => {
+    return <ColorCard 
+    colorInfo={color} 
+    key={color.color}/>
+  })
+
   return (
     <section className="color-container">
-      <ColorCard />
-      <ColorCard />
-      <ColorCard />
-      <ColorCard />
-      <ColorCard />
+      {allColorCards}
     </section>
   )
 }

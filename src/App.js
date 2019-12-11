4cb323959e2d7a58bfd7bdd303ae9cd0d27ca694
Eduardo <jeduardorjx@gmail.com>
2019-12-10 18:23:12 -0700

    Set a randomizeColor method

--- src/App.js
+++ src/App.js
@@ -5,18 +5,37 @@ import { Main } from '../src/containers/Main/Main';
 import { ColorContainer } from '../src/containers/ColorContainer/ColorContainer';
 
 export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      colors: []
+    }
+  }
 
   async componentDidMount() {
+    this.randomizeColors()
     await fetch(process.env.REACT_APP_BACKEND_URL + '/')
   }
 
+  randomizeColors = () => {
+    let colors = [];
+    while (colors.length < 5) {
+      let randomColor = '#000000'.replace(/0/g, () => {
+        return (~~(Math.random()*16)).toString(16)
+      })
+      colors.push({ color: randomColor, isLocked: false })
+    }
+    this.setState({ colors })
+  }
+
   render() {
+    const { colors } = this.state
     return (
       <div>
         <Header />
         <small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>
         <Main />
-        <ColorContainer />
+        <ColorContainer colors={colors}/>
       </div>
     )
   }

3aaa50ebe00b28a31a22c40c050d5999d05018fd
Eduardo <jeduardorjx@gmail.com>
2019-12-10 19:34:58 -0700

    Take advantage if the color is locked or not and display icon accordingly

--- src/containers/ColorCard/ColorCard.js
+++ src/containers/ColorCard/ColorCard.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './ColorCard.scss';
+import locked from '../../images/locked.svg';
+import unlocked from '../../images/unlocked.svg';
 
 export const ColorCard = ({ colorInfo }) => {
   const { color, isLocked} = colorInfo;
+  const icon = isLocked ? locked : unlocked;
+  const description = isLocked ? 'locked icon' : 'unlocked icon'
+  
   return (
     <article className="color-card"
       style={{backgroundColor: color}}>
-      
+      <button class='icon-btn'>
+        <img class='lock-icon' src={icon} alt={description} />
+      <h3 class='color-text'>{color.toUpperCase()}</h3>
+      </button>
     </article>
   )
 }
